47e469ad14ca5500921701b0392c9c848f31c996
Shota <tateishs@fsi.co.jp>
2025-09-01 16:16:50 +0900

    9月１日 Homecontroller javascript導入

--- TODO/src/main/java/com/example/todoapp/controller/TaskController.java
+++ TODO/src/main/java/com/example/todoapp/controller/TaskController.java
@@ -66,3 +66,6 @@ public class TaskController {
         return taskService.createTask(task);
     }
 }
+
+
+
--- TODO/src/main/java/com/example/todoapp/dto/TaskResponse.java
+++ TODO/src/main/java/com/example/todoapp/dto/TaskResponse.java
@@ -1,5 +1,53 @@
 package com.example.todoapp.dto;
 
 public class TaskResponse {
+    private Long id;
+    private String title;
+    private String description;
+    private String duedate;
+    private String status;
+    
+    // getter
+    private Long getId() {
+        return id;
+    }
 
-}
+    private String getTitle() {
+        return title;
+    }
+
+    private String getDescription() {
+        return description;
+    }
+
+    private String getDuedate() {
+        return duedate; 
+    }
+    private String getStatus() {
+        return status;
+    }
+
+    // setter
+    private void setId( Long id) {
+        this.id = id;
+    }
+
+    private void setTitle ( String title) {
+        this.title = title;
+    }
+
+    private void setDescription (String description) {
+        this.description = description;
+    }
+
+    private void setDuedate (String duedate) {
+        this.duedate = duedate;
+    }
+
+    private void setStatus (String status) {
+        this.status = status;
+    }
+
+    }
+
+
