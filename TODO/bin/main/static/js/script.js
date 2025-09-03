cdf2e2741845dba86c0fdeb01f2d6eb33d2530e4
Shota <tateishs@fsi.co.jp>
2025-09-03 10:47:07 +0900

    build・binファイル　プッシュ

---
+++ TODO/bin/main/static/js/script.js
@@ -0,0 +1,4 @@
+document.getElementById("loginBtn").addEventListener("click", function() {
+    const form = document.getElementById("loginForm");
+    form.style.display = form.style.display === "none" ? "block" : "none";
+});
